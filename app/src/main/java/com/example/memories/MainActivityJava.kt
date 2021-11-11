9a32fef61c5217e80e99f187b1e1325b3506d23c
Pri-cell <66022612+Pri-cell@users.noreply.github.com>
2021-11-11 19:06:39 -0400

    layout jeu

--- app/src/main/java/com/example/memories/MainActivityJava.kt
+++ app/src/main/java/com/example/memories/MainActivityJava.kt
@@ -6,29 +6,28 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import android.os.Bundle
 import android.view.View
 import com.example.memories.R
+import android.widget.TextView
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
-    private var gamePiece: GridLayout? = null
+class MainActivityJava : AppCompatActivity() {
+    private var gamePiece: ConstraintLayout? = null
     private var nbImg: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_piece)
-        gamePiece = findViewById<View>(R.id.MyDynamicLayout) as GridLayout
-
-        nbImg = 2
-
-
-
+        gamePiece = findViewById<View>(R.id.MyDynamicLayout) as ConstraintLayout
+        val text = TextView(this)
+        text.text = "Trouvez les paires"
+        text.textSize = 25f
+        gamePiece!!.addView(text)
+        nbImg = 4
         for (i in 0 until nbImg!!) {
             val image = ImageView(this)
             val params: ViewGroup.LayoutParams = ActionBar.LayoutParams(100, 100)
-            image.layoutParams=params
+            image.layoutParams = params
             image.setBackgroundResource(R.drawable.plus)
             gamePiece!!.addView(image)
         }
-
-
     }
 }
