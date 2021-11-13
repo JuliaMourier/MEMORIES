5d5057a2e16fff641ce577f89560e6d49eb7dfd9
Pepous <cyril.guirguis.06@gmail.com>
2021-11-12 21:45:42 -0500

    Ajout de la suppression des dossiers

--- app/src/main/java/com/example/memories/ShowFoldersActivity.java
+++ app/src/main/java/com/example/memories/ShowFoldersActivity.java
@@ -1,11 +1,8 @@
 package com.example.memories;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.Spanned;
@@ -19,9 +16,10 @@ import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -34,12 +32,13 @@ import java.util.ArrayList;
 public class ShowFoldersActivity extends AppCompatActivity {
     private static final String FILE_NAME = "foldersName.txt";
     GridLayout foldersGridLayout;
-    ImageButton addButton;
+    ImageButton addButton, generalDeleteButton;
     float density, dpHeight, dpWidth;
     int rowCount, columnCount,folderIconPxSize;
     ArrayList<String> foldersNameListOnStorage=new ArrayList<String>();
     ArrayList<String> foldersNameList=new ArrayList<String>();
-    AlertDialog.Builder builder;
+    AlertDialog.Builder builder, builder1;
+    ArrayList<ImageButton> deleteButtonList = new ArrayList<ImageButton>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,27 +57,70 @@ public class ShowFoldersActivity extends AppCompatActivity {
         folderIconPxSize = (int)(outMetrics.widthPixels/columnCount);
         this.foldersGridLayout= (GridLayout)findViewById(R.id.folder_grid);
         this.addButton = findViewById(R.id.add_button);
+        this.generalDeleteButton = findViewById(R.id.delete_button);
+        generalDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowFoldersActivity.this.setAllDeleteButtonsVisible();
+            }
+        });
         builder = new AlertDialog.Builder(this);
+        builder1 = new AlertDialog.Builder(this);
         int n = foldersNameListOnStorage.size();
         for(int i =0;i<n;i++){
             String folderNameText = foldersNameListOnStorage.get(i);
             ViewGroup.LayoutParams paramsFrameLayout = new ActionBar.LayoutParams(folderIconPxSize,folderIconPxSize);
+            FrameLayout.LayoutParams paramsDelete = new FrameLayout.LayoutParams((int)(folderIconPxSize*0.2), (int)(folderIconPxSize*0.2));
             FrameLayout.LayoutParams paramsIcon = new FrameLayout.LayoutParams((int)(folderIconPxSize*0.8), (int)(folderIconPxSize*0.8));
             FrameLayout.LayoutParams paramsText = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
             FrameLayout cellFolderIcon = new FrameLayout(this);
+            ImageButton deleteIcon = new ImageButton(this);
+            deleteIcon.setVisibility(View.INVISIBLE);
+            deleteButtonList.add(deleteIcon);
             ImageView folderIcon = new ImageView(this);
             TextView folderName = new TextView(this);
 
+            deleteIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    builder1.setMessage("Confirmer la suppression ?")
+                            .setCancelable(false)
+                            .setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    foldersGridLayout.removeView(cellFolderIcon);
+                                    foldersNameList.remove(folderNameText);
+                                    ShowFoldersActivity.this.saveFoldersNameOnStorage();
+                                    ShowFoldersActivity.this.setAllDeleteButtonsInvisible();
+                                }
+                            })
+                            .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    //Creating dialog box
+                    AlertDialog alert1 = builder1.create();
+                    //Setting the title manually
+                    alert1.setTitle("Suppression Dossier");
+                    alert1.getWindow().getAttributes().windowAnimations=R.style.MyDialogAnimation;
+                    alert1.show();
+                }
+            });
             cellFolderIcon.setLayoutParams(paramsFrameLayout);
             folderName.setText(folderNameText);
+            paramsDelete.gravity = Gravity.TOP|Gravity.RIGHT;
+            deleteIcon.setLayoutParams(paramsDelete);
             paramsText.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
             folderIcon.setLayoutParams(paramsIcon);
             paramsIcon.gravity = Gravity.CENTER_HORIZONTAL;
             folderIcon.setBackgroundResource(R.drawable.folder_icon);
+            deleteIcon.setBackgroundResource(R.drawable.minus);
 
             folderName.setLayoutParams(paramsText);
 
+            cellFolderIcon.addView(deleteIcon);
             cellFolderIcon.addView(folderIcon);
             cellFolderIcon.addView(folderName);
             this.foldersNameList.add(folderNameText);
@@ -108,6 +150,7 @@ public class ShowFoldersActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 activity.addFolder();
+
             }
         });
     }
@@ -120,22 +163,58 @@ public class ShowFoldersActivity extends AppCompatActivity {
                 String folderNameText = editText.getText().toString();
                 if(!folderNameText.equals("")){
                     ViewGroup.LayoutParams paramsFrameLayout = new ActionBar.LayoutParams(folderIconPxSize,folderIconPxSize);
+                    FrameLayout.LayoutParams paramsDelete = new FrameLayout.LayoutParams((int)(folderIconPxSize*0.2), (int)(folderIconPxSize*0.2));
                     FrameLayout.LayoutParams paramsIcon = new FrameLayout.LayoutParams((int)(folderIconPxSize*0.8), (int)(folderIconPxSize*0.8));
                     FrameLayout.LayoutParams paramsText = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
                     FrameLayout cellFolderIcon = new FrameLayout(activity);
+                    ImageButton deleteIcon = new ImageButton(activity);
+                    deleteIcon.setVisibility(View.INVISIBLE);
+                    activity.deleteButtonList.add(deleteIcon);
                     ImageView folderIcon = new ImageView(activity);
                     TextView folderName = new TextView(activity);
 
+                    deleteIcon.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            builder1.setMessage("Confirmer la suppression ?")
+                                    .setCancelable(false)
+                                    .setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            foldersGridLayout.removeView(cellFolderIcon);
+                                            foldersNameList.remove(folderNameText);
+                                            ShowFoldersActivity.this.saveFoldersNameOnStorage();
+                                            ShowFoldersActivity.this.setAllDeleteButtonsInvisible();
+                                        }
+                                    })
+                                    .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                            //Creating dialog box
+                            AlertDialog alert1 = builder1.create();
+                            //Setting the title manually
+                            alert1.setTitle("Suppression Dossier");
+                            alert1.getWindow().getAttributes().windowAnimations=R.style.MyDialogAnimation;
+                            alert1.show();
+                        }
+                    });
+
                     cellFolderIcon.setLayoutParams(paramsFrameLayout);
                     folderName.setText(folderNameText);
+                    paramsDelete.gravity = Gravity.TOP|Gravity.RIGHT;
+                    deleteIcon.setLayoutParams(paramsDelete);
                     paramsText.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
                     folderIcon.setLayoutParams(paramsIcon);
                     paramsIcon.gravity = Gravity.CENTER_HORIZONTAL;
                     folderIcon.setBackgroundResource(R.drawable.folder_icon);
+                    deleteIcon.setBackgroundResource(R.drawable.minus);
 
                     folderName.setLayoutParams(paramsText);
 
+                    cellFolderIcon.addView(deleteIcon);
                     cellFolderIcon.addView(folderIcon);
                     cellFolderIcon.addView(folderName);
                     activity.foldersNameList.add(folderNameText);
@@ -176,14 +255,14 @@ public class ShowFoldersActivity extends AppCompatActivity {
         }
         return textInFileFoldersNameString;
     }
-    public void saveFoldersNameOnStorage() {
+    protected void saveFoldersNameOnStorage() {
         String text = this.textInFileFoldersName();
         FileOutputStream fos = null;
         try {
             fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
             fos.write(text.getBytes());
-            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
-                    Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+              //      Toast.LENGTH_LONG).show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -198,7 +277,7 @@ public class ShowFoldersActivity extends AppCompatActivity {
             }
         }
     }
-    public void loadFoldersNameFromStorage() {
+    protected void loadFoldersNameFromStorage() {
         FileInputStream fis = null;
         try {
             fis = openFileInput(FILE_NAME);
@@ -223,4 +302,17 @@ public class ShowFoldersActivity extends AppCompatActivity {
             }
         }
     }
+    public void setAllDeleteButtonsVisible(){
+        int n = deleteButtonList.size();
+        for (int i=0;i<n;i++){
+            deleteButtonList.get(i).setVisibility(View.VISIBLE);
+        }
+    }
+    public void setAllDeleteButtonsInvisible(){
+        int n = deleteButtonList.size();
+        for (int i=0;i<n;i++){
+            deleteButtonList.get(i).setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
