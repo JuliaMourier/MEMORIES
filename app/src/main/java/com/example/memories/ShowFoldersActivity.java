21020f126c98e1dadc4a533b1a69172757ecb103
Pepous <cyril.guirguis.06@gmail.com>
2021-11-12 19:41:46 -0500

    Ajout stockage des dossiers

--- app/src/main/java/com/example/memories/ShowFoldersActivity.java
+++ app/src/main/java/com/example/memories/ShowFoldersActivity.java
@@ -23,16 +23,28 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class ShowFoldersActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "foldersName.txt";
     GridLayout foldersGridLayout;
     ImageButton addButton;
     float density, dpHeight, dpWidth;
     int rowCount, columnCount,folderIconPxSize;
+    ArrayList<String> foldersNameListOnStorage=new ArrayList<String>();
+    ArrayList<String> foldersNameList=new ArrayList<String>();
     AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_folders);
+        this.loadFoldersNameFromStorage();
         Display display = getWindowManager().getDefaultDisplay();
         DisplayMetrics outMetrics = new DisplayMetrics ();
         display.getMetrics(outMetrics);
@@ -47,6 +59,31 @@ public class ShowFoldersActivity extends AppCompatActivity {
         this.foldersGridLayout= (GridLayout)findViewById(R.id.folder_grid);
         this.addButton = findViewById(R.id.add_button);
         builder = new AlertDialog.Builder(this);
+        int n = foldersNameListOnStorage.size();
+        for(int i =0;i<n;i++){
+            String folderNameText = foldersNameListOnStorage.get(i);
+            ViewGroup.LayoutParams paramsFrameLayout = new ActionBar.LayoutParams(folderIconPxSize,folderIconPxSize);
+            FrameLayout.LayoutParams paramsIcon = new FrameLayout.LayoutParams((int)(folderIconPxSize*0.8), (int)(folderIconPxSize*0.8));
+            FrameLayout.LayoutParams paramsText = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            FrameLayout cellFolderIcon = new FrameLayout(this);
+            ImageView folderIcon = new ImageView(this);
+            TextView folderName = new TextView(this);
+
+            cellFolderIcon.setLayoutParams(paramsFrameLayout);
+            folderName.setText(folderNameText);
+            paramsText.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
+            folderIcon.setLayoutParams(paramsIcon);
+            paramsIcon.gravity = Gravity.CENTER_HORIZONTAL;
+            folderIcon.setBackgroundResource(R.drawable.folder_icon);
+
+            folderName.setLayoutParams(paramsText);
+
+            cellFolderIcon.addView(folderIcon);
+            cellFolderIcon.addView(folderName);
+            this.foldersNameList.add(folderNameText);
+            foldersGridLayout.addView(cellFolderIcon);
+        }
         this.setOnClick(addButton,this);
 
     }
@@ -101,7 +138,9 @@ public class ShowFoldersActivity extends AppCompatActivity {
 
                     cellFolderIcon.addView(folderIcon);
                     cellFolderIcon.addView(folderName);
+                    activity.foldersNameList.add(folderNameText);
                     foldersGridLayout.addView(cellFolderIcon);
+                    activity.saveFoldersNameOnStorage();
                 }
                 else{
                     builder.setMessage("Erreur : le nom doit au moins contenir 1 caractère");
@@ -128,5 +167,60 @@ public class ShowFoldersActivity extends AppCompatActivity {
 
         , new InputFilter.LengthFilter(10)});
         editText.setMaxLines(1);
+    }
+    protected String textInFileFoldersName(){
+        int n = foldersNameList.size();
+        String textInFileFoldersNameString ="";
+        for(int i=0;i<n;i++){
+            textInFileFoldersNameString+=foldersNameList.get(i)+"\n";
+        }
+        return textInFileFoldersNameString;
+    }
+    public void saveFoldersNameOnStorage() {
+        String text = this.textInFileFoldersName();
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void loadFoldersNameFromStorage() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                foldersNameListOnStorage.add(text);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
