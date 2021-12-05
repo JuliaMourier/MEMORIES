531e9e5414f8a35d257dbfe0fb0b3c4196d818c9
OctoJul <julia.mourier@isen.yncrea.fr>
2021-12-04 22:18:30 -0500

    Permission Camera asking to user

--- app/src/main/java/com/example/memories/QRCode/QRCodeScanner.kt
+++ app/src/main/java/com/example/memories/QRCode/QRCodeScanner.kt
@@ -1,5 +1,6 @@
 package com.example.memories.QRCode
 
+import android.Manifest
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -14,16 +15,29 @@ import com.budiyev.android.codescanner.ScanMode
 import com.example.memories.Database.GetFirebaseDataActivity
 import com.example.memories.NumberCardActivity
 import com.example.memories.R
+import android.content.pm.PackageManager
+
+import androidx.annotation.NonNull
+
+
+
 
 class QRCodeScanner : AppCompatActivity() {
     //THIS CODE HAS BEEN TAKEN FROM
     // https://github.com/yuriy-budiyev/code-scanner
     // https://www.geeksforgeeks.org/shared-preferences-in-android-with-examples/
+    //https://stackoverflow.com/questions/38552144/how-get-permission-for-camera-in-android-specifically-marshmallow
     private lateinit var codeScanner: CodeScanner
+    private val MY_CAMERA_REQUEST_CODE = 100
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_qrcode_scanner)
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), MY_CAMERA_REQUEST_CODE)
+        }
         val scannerView: CodeScannerView = findViewById(R.id.scanner_view)
 
         codeScanner = CodeScanner(this, scannerView)
@@ -66,6 +80,24 @@ class QRCodeScanner : AppCompatActivity() {
             codeScanner.startPreview()
         }
     }
+
+    //When the user has given his answer on the camera permission
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {//If the permission is granted
+                Toast.makeText(this, "Permission accordée", Toast.LENGTH_LONG).show()
+            } else { //If its denied
+                Toast.makeText(this, "Permission refusée", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 
     override fun onResume() {
         super.onResume()
